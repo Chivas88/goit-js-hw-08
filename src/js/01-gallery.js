43780f229dc226fa36625dcbdde5dbb87f8e0cb8
Chivas88 <marinasavich88@gmail.com>
2022-07-10 19:51:14 +0200

    Выполнение 8-го дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const imageGallery = document.querySelector('.gallery');
+renderGallery();
+
+function renderGallery() {
+    const markup = galleryItems.map(image => `
+    <div class="gallery__item">
+    <a class="gallery__item" href="${image.original}">
+  <img
+   class="gallery__image"
+   src="${image.preview}" 
+   alt="${image.description}" />
+</a>
+</div>
+    ` )
+        .join('');
+    imageGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+let gallery = new SimpleLightbox('.gallery a',{
+    captionsData : 'alt' ,
+    animationSpeed: 250
+} ) ;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form'); 
+initform();
+    
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    formData.forEach((key, value) => console.log(`${key} - ${value}`))
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+);
+form.addEventListener('input', throttle(event => {
+    let parsedFilters = localStorage.getItem('feedback-form-state');
+    
+    parsedFilters = parsedFilters ? JSON.parse(parsedFilters) : {};
+    parsedFilters[event.target.name] = event.target.value;
 
+    if (parsedFilters) {
+        localStorage.setItem('feedback-form-state', JSON.stringify(parsedFilters));
+    };
+},500));
+function initform() {
+    let parsedFilters = localStorage.getItem('feedback-form-state');
+    parsedFilters = parsedFilters ? JSON.parse(parsedFilters) : {};
+    
+    if (parsedFilters) {
+        Object.entries(parsedFilters).forEach(( [ name, value ]) => form.elements[name].value = value);
+    }
+    
+}
